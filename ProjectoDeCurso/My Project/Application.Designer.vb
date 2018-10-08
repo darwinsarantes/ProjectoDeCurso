a02dd334457105abad4c1ae7001cff25067cf320
darwinsarantes <43971555+darwinsarantes@users.noreply.github.com>
2018-10-08 14:56:54 -0600

    Guardar las nuevas ventanas

--- ProjectoDeCurso/My Project/Application.Designer.vb
+++ ProjectoDeCurso/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ProjectoDeCurso.Form1
+            Me.MainForm = Global.ProjectoDeCurso.frmSQLConexion
         End Sub
     End Class
 End Namespace
