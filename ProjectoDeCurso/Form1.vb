bcfa42468af7fd694e93990075a1676194c0db4b
darwinsarantes <43971555+darwinsarantes@users.noreply.github.com>
2018-10-08 14:58:28 -0600

    v

--- ProjectoDeCurso/Form1.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class frmSQLConexion
-
-End Class
---
+++ ProjectoDeCurso/frmSQLConexion.vb
@@ -0,0 +1,5 @@
+﻿Public Class frmSQLConexion
+    Private Sub frmSQLConexion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
