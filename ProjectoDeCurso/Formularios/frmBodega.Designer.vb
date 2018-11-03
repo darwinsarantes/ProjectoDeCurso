1d95ca8019ae3babc6c995fef74f55396e5502da
Darwin R. Sarantes <Darwin R. Sarantes@DESKTOP-0PPKGB3>
2018-11-03 07:19:04 -0600

    Desarrollo de formulario de bodega

---
+++ ProjectoDeCurso/Formularios/frmBodega.Designer.vb
@@ -0,0 +1,431 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBodega
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmBodega))
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCodigoBodega = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.dgvListar = New System.Windows.Forms.DataGridView()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStrip2 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.tsbFiltro = New System.Windows.Forms.ToolStripButton()
+        Me.cmsMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.NuevoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ModificarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EliminarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VisualizarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ImprimirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.chkCodigoDeBodega = New System.Windows.Forms.CheckBox()
+        Me.txtFCodigoDeBodega = New System.Windows.Forms.TextBox()
+        Me.txtFNombre = New System.Windows.Forms.TextBox()
+        Me.chkNombre = New System.Windows.Forms.CheckBox()
+        Me.txtFObservaciones = New System.Windows.Forms.TextBox()
+        Me.chkObservaciones = New System.Windows.Forms.CheckBox()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer2.Panel1.SuspendLayout()
+        Me.SplitContainer2.Panel2.SuspendLayout()
+        Me.SplitContainer2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dgvListar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ToolStrip1.SuspendLayout()
+        Me.ToolStrip2.SuspendLayout()
+        Me.cmsMenu.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.ToolStrip2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.GroupBox1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.SplitContainer2)
+        Me.SplitContainer1.Size = New System.Drawing.Size(913, 382)
+        Me.SplitContainer1.SplitterDistance = 363
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'SplitContainer2
+        '
+        Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer2.Name = "SplitContainer2"
+        Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer2.Panel1
+        '
+        Me.SplitContainer2.Panel1.Controls.Add(Me.GroupBox2)
+        Me.SplitContainer2.Panel1.Controls.Add(Me.ToolStrip1)
+        '
+        'SplitContainer2.Panel2
+        '
+        Me.SplitContainer2.Panel2.Controls.Add(Me.dgvListar)
+        Me.SplitContainer2.Size = New System.Drawing.Size(546, 382)
+        Me.SplitContainer2.SplitterDistance = 182
+        Me.SplitContainer2.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtCantidad)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtNombre)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtCodigoBodega)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(3, 54)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(357, 405)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Información del registro"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Código de Bodega:"
+        '
+        'txtCodigoBodega
+        '
+        Me.txtCodigoBodega.Location = New System.Drawing.Point(113, 13)
+        Me.txtCodigoBodega.Name = "txtCodigoBodega"
+        Me.txtCodigoBodega.Size = New System.Drawing.Size(185, 20)
+        Me.txtCodigoBodega.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Nombre de Bodega:"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(113, 39)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(185, 20)
+        Me.txtNombre.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Cantidad:"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(113, 65)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(185, 20)
+        Me.txtCantidad.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 94)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Observaciones:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 110)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(286, 84)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 203)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(63, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Descripción"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 219)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(286, 102)
+        Me.TextBox2.TabIndex = 1
+        '
+        'dgvListar
+        '
+        Me.dgvListar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvListar.ContextMenuStrip = Me.cmsMenu
+        Me.dgvListar.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvListar.Location = New System.Drawing.Point(0, 0)
+        Me.dgvListar.Name = "dgvListar"
+        Me.dgvListar.Size = New System.Drawing.Size(546, 196)
+        Me.dgvListar.TabIndex = 0
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbFiltro})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 151)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(546, 31)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStrip2
+        '
+        Me.ToolStrip2.ImageScalingSize = New System.Drawing.Size(32, 32)
+        Me.ToolStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1})
+        Me.ToolStrip2.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip2.Name = "ToolStrip2"
+        Me.ToolStrip2.Size = New System.Drawing.Size(363, 39)
+        Me.ToolStrip2.TabIndex = 1
+        Me.ToolStrip2.Text = "ToolStrip2"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(36, 36)
+        Me.ToolStripButton1.Text = "ToolStripButton1"
+        '
+        'tsbFiltro
+        '
+        Me.tsbFiltro.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbFiltro.Image = CType(resources.GetObject("tsbFiltro.Image"), System.Drawing.Image)
+        Me.tsbFiltro.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbFiltro.Name = "tsbFiltro"
+        Me.tsbFiltro.Size = New System.Drawing.Size(28, 28)
+        Me.tsbFiltro.Text = "Buscar Listado"
+        '
+        'cmsMenu
+        '
+        Me.cmsMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoToolStripMenuItem, Me.ModificarToolStripMenuItem, Me.EliminarToolStripMenuItem, Me.VisualizarToolStripMenuItem, Me.ImprimirToolStripMenuItem})
+        Me.cmsMenu.Name = "cmsMenu"
+        Me.cmsMenu.Size = New System.Drawing.Size(126, 114)
+        '
+        'NuevoToolStripMenuItem
+        '
+        Me.NuevoToolStripMenuItem.Name = "NuevoToolStripMenuItem"
+        Me.NuevoToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.NuevoToolStripMenuItem.Text = "Nuevo"
+        '
+        'ModificarToolStripMenuItem
+        '
+        Me.ModificarToolStripMenuItem.Name = "ModificarToolStripMenuItem"
+        Me.ModificarToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.ModificarToolStripMenuItem.Text = "Modificar"
+        '
+        'EliminarToolStripMenuItem
+        '
+        Me.EliminarToolStripMenuItem.Name = "EliminarToolStripMenuItem"
+        Me.EliminarToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.EliminarToolStripMenuItem.Text = "Eliminar"
+        '
+        'VisualizarToolStripMenuItem
+        '
+        Me.VisualizarToolStripMenuItem.Name = "VisualizarToolStripMenuItem"
+        Me.VisualizarToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.VisualizarToolStripMenuItem.Text = "Visualizar"
+        '
+        'ImprimirToolStripMenuItem
+        '
+        Me.ImprimirToolStripMenuItem.Name = "ImprimirToolStripMenuItem"
+        Me.ImprimirToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.ImprimirToolStripMenuItem.Text = "Imprimir"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.chkObservaciones)
+        Me.GroupBox2.Controls.Add(Me.chkNombre)
+        Me.GroupBox2.Controls.Add(Me.chkCodigoDeBodega)
+        Me.GroupBox2.Controls.Add(Me.txtFObservaciones)
+        Me.GroupBox2.Controls.Add(Me.txtFNombre)
+        Me.GroupBox2.Controls.Add(Me.txtFCodigoDeBodega)
+        Me.GroupBox2.Location = New System.Drawing.Point(3, 9)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(540, 139)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Filtro de información para bodega"
+        '
+        'chkCodigoDeBodega
+        '
+        Me.chkCodigoDeBodega.AutoSize = True
+        Me.chkCodigoDeBodega.Location = New System.Drawing.Point(6, 19)
+        Me.chkCodigoDeBodega.Name = "chkCodigoDeBodega"
+        Me.chkCodigoDeBodega.Size = New System.Drawing.Size(114, 17)
+        Me.chkCodigoDeBodega.TabIndex = 0
+        Me.chkCodigoDeBodega.Text = "Código de Bodega"
+        Me.chkCodigoDeBodega.UseVisualStyleBackColor = True
+        '
+        'txtFCodigoDeBodega
+        '
+        Me.txtFCodigoDeBodega.Location = New System.Drawing.Point(126, 17)
+        Me.txtFCodigoDeBodega.Name = "txtFCodigoDeBodega"
+        Me.txtFCodigoDeBodega.Size = New System.Drawing.Size(185, 20)
+        Me.txtFCodigoDeBodega.TabIndex = 1
+        '
+        'txtFNombre
+        '
+        Me.txtFNombre.Location = New System.Drawing.Point(126, 43)
+        Me.txtFNombre.Name = "txtFNombre"
+        Me.txtFNombre.Size = New System.Drawing.Size(185, 20)
+        Me.txtFNombre.TabIndex = 1
+        '
+        'chkNombre
+        '
+        Me.chkNombre.AutoSize = True
+        Me.chkNombre.Location = New System.Drawing.Point(6, 45)
+        Me.chkNombre.Name = "chkNombre"
+        Me.chkNombre.Size = New System.Drawing.Size(66, 17)
+        Me.chkNombre.TabIndex = 0
+        Me.chkNombre.Text = "Bodega:"
+        Me.chkNombre.UseVisualStyleBackColor = True
+        '
+        'txtFObservaciones
+        '
+        Me.txtFObservaciones.Location = New System.Drawing.Point(126, 69)
+        Me.txtFObservaciones.Name = "txtFObservaciones"
+        Me.txtFObservaciones.Size = New System.Drawing.Size(185, 20)
+        Me.txtFObservaciones.TabIndex = 1
+        '
+        'chkObservaciones
+        '
+        Me.chkObservaciones.AutoSize = True
+        Me.chkObservaciones.Location = New System.Drawing.Point(6, 71)
+        Me.chkObservaciones.Name = "chkObservaciones"
+        Me.chkObservaciones.Size = New System.Drawing.Size(97, 17)
+        Me.chkObservaciones.TabIndex = 0
+        Me.chkObservaciones.Text = "Observaciones"
+        Me.chkObservaciones.UseVisualStyleBackColor = True
+        '
+        'frmBodega
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(913, 382)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "frmBodega"
+        Me.Text = "frmBodega"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.PerformLayout()
+        Me.SplitContainer2.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer2.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.dgvListar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ToolStrip2.ResumeLayout(False)
+        Me.ToolStrip2.PerformLayout()
+        Me.cmsMenu.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents SplitContainer2 As SplitContainer
+    Friend WithEvents ToolStrip2 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCodigoBodega As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents tsbFiltro As ToolStripButton
+    Friend WithEvents dgvListar As DataGridView
+    Friend WithEvents cmsMenu As ContextMenuStrip
+    Friend WithEvents NuevoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ModificarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EliminarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VisualizarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ImprimirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents chkObservaciones As CheckBox
+    Friend WithEvents chkNombre As CheckBox
+    Friend WithEvents chkCodigoDeBodega As CheckBox
+    Friend WithEvents txtFObservaciones As TextBox
+    Friend WithEvents txtFNombre As TextBox
+    Friend WithEvents txtFCodigoDeBodega As TextBox
+End Class
---
+++ ProjectoDeCurso/Formularios/frmBodega.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmBodega
+
+End Class
